004b0a1582c2fd050109d801d54ad869f55baf63
Adxmn <adamdanixl@gmail.com>
2023-09-05 12:45:34 +0800

    exercise

---
+++ main.py
@@ -0,0 +1,10 @@
+def print_hi():
+    pelajar = {"Name": "Adam", "Age": 20, "nomatrix": "18DDT21F2033"}
+
+    print(pelajar["Name"])
+    print(pelajar["Age"])
+    print(pelajar["nomatrix"])
+
+
+if __name__ == '__main__':
+    print_hi()
